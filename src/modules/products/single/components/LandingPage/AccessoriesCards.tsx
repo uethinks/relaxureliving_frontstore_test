19bf10b29f65cb887d7dc74d4d35e0520f2b41d1
huangpeipei <huang.peipei@system-in-motion.com>
2025-04-12 14:37:59 +0800

    product information UI update

--- src/modules/products/single/components/LandingPage/AccessoriesCards.tsx
+++ src/modules/products/single/components/LandingPage/AccessoriesCards.tsx
@@ -2,24 +2,24 @@ import React from "react"
 
 export const AccessoriesCards = (): JSX.Element => {
   return (
-    <div className="flex flex-col w-full items-center justify-center gap-10 relative">
+    <div className="flex flex-col w-full items-center justify-center gap-10 relative mt-10">
       <div className="flex flex-col w-full items-center gap-5 relative flex-[0_0_auto]">
         <div className="flex flex-col w-full h-[100px] items-start relative">
           <div className="flex items-center justify-center gap-2.5 px-2.5 py-0 relative self-stretch w-full flex-[0_0_auto]">
-            <div className="relative w-[1124px] mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-[#343a40] text-[length:var(--heading-2-font-size)] text-center tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
+            <div className="relative w-full mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-[#343a40] text-[length:var(--heading-2-font-size)] text-center tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
               Your patio, your way
             </div>
           </div>
 
           <div className="flex items-center justify-center gap-2.5 px-2.5 py-0 relative self-stretch w-full flex-[0_0_auto]">
-            <div className="relative w-[1124px] mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-[#343a40] text-[length:var(--heading-2-font-size)] text-center tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
+            <div className="relative w-full mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-[#343a40] text-[length:var(--heading-2-font-size)] text-center tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
               Accessories and customizations
             </div>
           </div>
         </div>
 
         <div className="inline-flex items-center justify-center gap-2.5 px-2.5 py-0 relative flex-[0_0_auto]">
-          <p className="w-[657px] text-[#68717a] text-center relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+          <p className="w-full px-10 text-[#68717a] text-center relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
             From intimate dinners to extended family celebrations, in any
             season, the Relaxure Corsica can change and adapt to your needs on
             any occasion. Explore our range of accessories
@@ -27,11 +27,11 @@ export const AccessoriesCards = (): JSX.Element => {
         </div>
       </div>
 
-      <div className="flex items-center gap-10 relative self-stretch w-full flex-[0_0_auto]">
-        <div className="relative w-[428px] h-[600px] rounded-[20px] overflow-hidden bg-[url(https://c.animaapp.com/1ZQaGn9E/img/rectangle-1276.svg)] bg-cover bg-[50%_50%]">
-          <div className="relative h-[668px] top-[-68px] rounded-[20px]">
-            <div className="flex flex-col w-[373px] h-[136px] items-center gap-5 relative top-[456px] left-[27px] bg-[#ffffff3d] rounded-[20px] border border-solid border-[#ffffff70] backdrop-blur-[14.1px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(14.1px)_brightness(100%)]">
-              <div className="flex flex-col h-[136px] items-center gap-2 p-2.5 relative self-stretch w-full">
+      <div className="flex flex-col lg:flex-row items-center gap-10 relative self-stretch w-full flex-[0_0_auto] px-10 lg:px-0">
+        <div className="flex flex-col items-center justify-end relative w-full lg:w-1/3 h-[600px] rounded-[20px] overflow-hidden bg-[url(https://c.animaapp.com/1ZQaGn9E/img/rectangle-1276.svg)] bg-cover bg-[50%_50%]">
+          <div className="flex justify-center items-center relative rounded-[20px] mb-10">
+            <div className="flex flex-col w-[90%] items-center gap-5 relative bg-[#ffffff3d] rounded-[20px] border border-solid border-[#ffffff70] backdrop-blur-[14.1px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(14.1px)_brightness(100%)]">
+              <div className="flex flex-col items-center gap-2 p-2.5 relative self-stretch w-full">
                 <div className="flex items-center justify-center gap-2.5 relative self-stretch w-full flex-[0_0_auto]">
                   <div className="relative flex-1 mt-[-1.00px] [font-family:'Montserrat',Helvetica] font-bold text-[#ffffff] text-lg tracking-[0] leading-[25.2px]">
                     Side screens for sunshine
@@ -39,7 +39,7 @@ export const AccessoriesCards = (): JSX.Element => {
                 </div>
 
                 <div className="flex items-center gap-2.5 relative self-stretch w-full flex-[0_0_auto]">
-                  <p className="w-[408px] mr-[-55.00px] text-[#ffffff] relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+                  <p className="w-full mr-[-55.00px] text-[#ffffff] relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
                     They protect against harsh rays, strong gusts, pesky bugs,
                     and prying eyes to extend the time you spend outdoors.
                   </p>
@@ -49,10 +49,10 @@ export const AccessoriesCards = (): JSX.Element => {
           </div>
         </div>
 
-        <div className="relative w-[428px] h-[600px] rounded-[20px] overflow-hidden bg-[url(https://c.animaapp.com/1ZQaGn9E/img/rectangle-1276-1.svg)] bg-cover bg-[50%_50%]">
-          <div className="relative h-[668px] top-[-68px] rounded-[20px]">
-            <div className="flex flex-col w-[373px] h-[136px] items-center gap-5 relative top-[456px] left-[27px] bg-[#ffffff3d] rounded-[20px] border border-solid border-[#ffffff70] backdrop-blur-[14.1px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(14.1px)_brightness(100%)]">
-              <div className="flex flex-col h-[136px] items-center gap-2 p-2.5 relative self-stretch w-full">
+        <div className="flex flex-col justify-end items-center relative w-full lg:w-1/3 h-[600px] rounded-[20px] overflow-hidden bg-[url(https://c.animaapp.com/1ZQaGn9E/img/rectangle-1276-1.svg)] bg-cover bg-[50%_50%]">
+          <div className="flex justify-center items-center relative rounded-[20px] mb-10">
+            <div className="flex flex-col w-[90%] items-center gap-5 relative bg-[#ffffff3d] rounded-[20px] border border-solid border-[#ffffff70] backdrop-blur-[14.1px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(14.1px)_brightness(100%)]">
+              <div className="flex flex-col items-center gap-2 p-2.5 relative self-stretch w-full">
                 <div className="flex items-center justify-center gap-2.5 relative self-stretch w-full flex-[0_0_auto]">
                   <div className="font-bold relative flex-1 mt-[-1.00px] [font-family:'Montserrat',Helvetica] text-[#ffffff] text-lg tracking-[0] leading-[27px]">
                     Smart high-efficiency heaters
@@ -60,7 +60,7 @@ export const AccessoriesCards = (): JSX.Element => {
                 </div>
 
                 <div className="flex items-center gap-2.5 relative self-stretch w-full flex-[0_0_auto]">
-                  <p className="w-[346px] text-[#ffffff] relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+                  <p className="w-full text-[#ffffff] relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
                     Our discreet heating elements turn crisp fall evenings and
                     winter afternoons into bonus outdoor time.
                   </p>
@@ -70,10 +70,10 @@ export const AccessoriesCards = (): JSX.Element => {
           </div>
         </div>
 
-        <div className="relative w-[428px] h-[600px] rounded-[20px] overflow-hidden bg-[url(https://c.animaapp.com/1ZQaGn9E/img/rectangle-1276-2.svg)] bg-cover bg-[50%_50%]">
-          <div className="relative h-[668px] top-[-68px] rounded-[20px]">
-            <div className="flex flex-col w-[373px] h-[136px] items-center gap-5 relative top-[456px] left-[27px] bg-[#ffffff3d] rounded-[20px] border border-solid border-[#ffffff70] backdrop-blur-[14.1px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(14.1px)_brightness(100%)]">
-              <div className="flex flex-col h-[136px] items-center gap-2 p-2.5 relative self-stretch w-full">
+        <div className="flex flex-col justify-end items-center relative w-full lg:w-1/3 h-[600px] rounded-[20px] overflow-hidden bg-[url(https://c.animaapp.com/1ZQaGn9E/img/rectangle-1276-2.svg)] bg-cover bg-[50%_50%]">
+          <div className="flex justify-center items-center relative rounded-[20px] mb-10">
+            <div className="flex flex-col w-[90%] items-center gap-5 relative bg-[#ffffff3d] rounded-[20px] border border-solid border-[#ffffff70] backdrop-blur-[14.1px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(14.1px)_brightness(100%)]">
+              <div className="flex flex-col items-center gap-2 p-2.5 relative self-stretch w-full">
                 <div className="flex items-center justify-center gap-2.5 relative self-stretch w-full flex-[0_0_auto]">
                   <div className="font-semibold relative flex-1 mt-[-1.00px] [font-family:'Montserrat',Helvetica] text-[#ffffff] text-lg tracking-[0] leading-[27px]">
                     Frameless glass doors
@@ -81,7 +81,7 @@ export const AccessoriesCards = (): JSX.Element => {
                 </div>
 
                 <div className="flex items-center gap-2.5 relative self-stretch w-full flex-[0_0_auto]">
-                  <p className="w-[358px] mr-[-5.00px] text-[#ffffff] relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+                  <p className="w-full mr-[-5.00px] text-[#ffffff] relative mt-[-1.00px] font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
                     Add glass doors to completely transform your pergola into an
                     elegant open room you can always enjoy
                   </p>
--- src/modules/products/single/components/LandingPage/Advantages.tsx
+++ src/modules/products/single/components/LandingPage/Advantages.tsx
@@ -2,13 +2,13 @@ import React from "react"
 
 export const Advantages = (): JSX.Element => {
   return (
-    <div className="flex flex-col w-full items-center justify-center px-[120px] py-[100px] relative bg-[#f3f3f3]">
+    <div className="flex flex-col w-full items-center justify-center px-8 py-6 relative bg-[#f3f3f3] mt-10">
       <div className="flex flex-col items-center gap-[60px] relative self-stretch w-full flex-[0_0_auto]">
-        <p className="relative self-stretch [font-family:'Merriweather',Helvetica] font-bold text-[#343a40] text-[36px] text-center tracking-[0] leading-[50.4px]">
+        <p className="relative self-stretch [font-family:'Merriweather',Helvetica] font-bold text-[#343a40] text-[24px] text-center tracking-[0] leading-[50.4px]">
           Not just a pretty face — Relaxure Corsica is smart and reliable
         </p>
-        <div className="inline-flex items-start gap-[60px] relative flex-[0_0_auto]">
-          <div className="flex flex-col w-[516px] items-start gap-[20px] relative">
+        <div className="flex flex-col lg:flex-row items-start gap-[60px] relative flex-[0_0_auto]">
+          <div className="flex flex-col w-full lg:1/2 items-start gap-[20px] relative">
             <div className="flex flex-col items-start gap-[10px] relative self-stretch w-full flex-[0_0_auto]">
               <p className="relative self-stretch [font-family:'Merriweather',Helvetica] font-bold text-[#343a40] text-[22px] tracking-[0] leading-[30.8px]">
                 Smart tech with simple controls
@@ -19,10 +19,10 @@ export const Advantages = (): JSX.Element => {
                 controls and intelligent presets for every occasion.
               </p>
             </div>
-            <div className="flex flex-col w-[516px] items-start gap-[10px] p-[20px] relative bg-white rounded-[20px]">
+            <div className="flex flex-col w-full items-start gap-[10px] p-[20px] relative bg-white rounded-[20px]">
               <div className="inline-flex items-center gap-[20px] relative self-stretch w-full flex-[0_0_auto]">
                 <img
-                  className="relative w-[30px] h-[30px]"
+                  className="relative w-[24px] h-[24px]"
                   alt="Frame"
                   src="https://c.animaapp.com/m9bjqr4zF8Pf0s/img/frame-1000004890.svg"
                 />
@@ -32,7 +32,7 @@ export const Advantages = (): JSX.Element => {
               </div>
               <div className="inline-flex items-center gap-[20px] relative self-stretch w-full flex-[0_0_auto]">
                 <img
-                  className="relative w-[30px] h-[30px]"
+                  className="relative w-[24px] h-[24px]"
                   alt="Group"
                   src="https://c.animaapp.com/m9bjqr4zF8Pf0s/img/group.png"
                 />
@@ -42,7 +42,7 @@ export const Advantages = (): JSX.Element => {
               </div>
               <div className="inline-flex items-center gap-[20px] relative self-stretch w-full flex-[0_0_auto]">
                 <img
-                  className="relative w-[30px] h-[30px]"
+                  className="relative w-[24px] h-[24px]"
                   alt="Group"
                   src="https://c.animaapp.com/m9bjqr4zF8Pf0s/img/group-1.png"
                 />
@@ -72,7 +72,7 @@ export const Advantages = (): JSX.Element => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-[516px] items-start gap-[20px] relative">
+          <div className="flex flex-col w-full lg:1/2 items-start gap-[20px] relative">
             <div className="flex flex-col items-start gap-[10px] relative self-stretch w-full flex-[0_0_auto]">
               <p className="relative self-stretch [font-family:'Merriweather',Helvetica] font-bold text-[#343a40] text-[22px] tracking-[0] leading-[30.8px]">
                 Superior quality at a reasonable price
@@ -83,7 +83,7 @@ export const Advantages = (): JSX.Element => {
                 years to come.
               </p>
             </div>
-            <div className="flex flex-col w-[516px] items-start gap-[10px] p-[20px] relative bg-white rounded-[20px]">
+            <div className="flex flex-col w-full items-start gap-[10px] p-[20px] relative bg-white rounded-[20px]">
               <div className="inline-flex items-center gap-[20px] relative self-stretch w-full flex-[0_0_auto]">
                 <img
                   className="relative w-[24px] h-[24px]"
@@ -96,7 +96,7 @@ export const Advantages = (): JSX.Element => {
               </div>
               <div className="inline-flex items-center gap-[20px] relative self-stretch w-full flex-[0_0_auto]">
                 <img
-                  className="relative w-[30px] h-[30px]"
+                  className="relative w-[24px] h-[24px]"
                   alt="Group"
                   src="https://c.animaapp.com/m9bjqr4zF8Pf0s/img/group-2.png"
                 />
