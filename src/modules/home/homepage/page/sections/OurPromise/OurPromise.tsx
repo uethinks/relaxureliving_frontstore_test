d18a4123f3f3acf2e9c2dc4ae7ab3522e2b5c934
huangpeipei <huang.peipei@system-in-motion.com>
2025-04-18 10:53:55 +0800

    font size adjustment

--- src/modules/home/homepage/page/sections/OurPromise/OurPromise.tsx
+++ src/modules/home/homepage/page/sections/OurPromise/OurPromise.tsx
@@ -9,10 +9,10 @@ const PromiseCard: React.FC<PromiseCardProps> = ({ icon, description }) => {
   return (
     <div className="flex flex-col gap-4 p-6 w-full rounded-lg">
       <img src={icon} alt="Feature icon" className="w-12 h-12" />
-      <h3 className="text-[#343a40] text-xl font-semibold">
+      <h3 className="text-[#343a40] text-[20px] font-semibold">
         We are the gift that keeps giving
       </h3>
-      <p className="text-[#68717a] text-base">{description}</p>
+      <p className="text-[#68717a] text-[14px]">{description}</p>
     </div>
   )
 }
@@ -47,10 +47,10 @@ export const OurPromise = (): JSX.Element => {
         <div className="flex flex-col lg:gap-[132px] md:items-center lg:flex-row lg:items-center lg:justify-between">
           {/* Left Section */}
           <div className="flex flex-col gap-5 w-full md:w-[600px] lg:w-1/3 mb-10 lg:mb-0">
-            <h2 className="text-[#343a40] text-3xl font-bold">
+            <h2 className="text-[#343a40] text-[36px] font-bold">
               The Boring But Important Stuff
             </h2>
-            <p className="text-[#68717a] text-lg">
+            <p className="text-[#68717a] text-[18px]">
               These details might not be flashy, but they're why you'll love
               your Relaxure pergola from the day it arrives and for decades
               more!
