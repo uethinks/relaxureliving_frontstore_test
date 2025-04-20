37e4f87585a7f3eb1e0aca20fb275c2d5151e0ec
huangpeipei <huang.peipei@system-in-motion.com>
2025-04-20 08:44:02 +0800

    product page update

--- src/modules/products/single/components/FirstScreen.tsx
+++ src/modules/products/single/components/FirstScreen.tsx
@@ -75,8 +75,8 @@ export const FirstScreen: React.FC<FirstScreenProps> = ({
       <NavBarWrapper isFixed={false} />
       <div className="lg:mx-auto flex flex-col  bg-[#ffffff] w-full relative">
         <div className="flex flex-col w-full lg:flex-row justify-between items-start">
-          <div className="flex flex-col w-full lg:max-w-[817px] gap-5">
-            <div className="flex flex-col w-full items-center gap-5">
+          <div className="flex flex-col w-full  gap-5">
+            <div className="flex flex-col w-full lg:max-w-[817px] items-center gap-5">
               <div className="flex flex-col items-start relative self-stretch w-full">
                 <div className="relative mb-[24px] self-stretch mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-[#343a40] text-[24px] lg:text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
                   {product.title}
@@ -92,7 +92,7 @@ export const FirstScreen: React.FC<FirstScreenProps> = ({
               </div>
             </div>
             <div className="flex flex-row justify-between items-start relative w-full">
-              <div className="w-full relative flex flex-col">
+              <div className="w-full lg:max-w-[817px] relative flex flex-col">
                 <div className="flex flex-row justify-between">
                   <ImgContent product={product} property1="default" />
                 </div>
@@ -148,26 +148,24 @@ export const FirstScreen: React.FC<FirstScreenProps> = ({
                   )}
                 </div>
               </div>
+              {/* Desktop View */}
+              <ProductSelector
+                product={product}
+                accessories={accessories}
+                onVariantChange={onVariantChange}
+                onAccessoryToggle={onAccessoryToggle}
+                selectedVariant={selectedVariant}
+                selectedAccessoriesHeater={selectedAccessoriesHeater}
+                selectedAccessoriesShades={selectedAccessoriesShades}
+                selectedAccessoriesGlassdoor={selectedAccessoriesGlassdoor}
+                pergolaSize={pergolaSize}
+                pergolaQuantity={pergolaQuantity}
+                totalPrice={totalPrice}
+                onQuantityChange={onQuantityChange}
+                onBuyNow={onBuyNow}
+                isMobile={false}
+              />
             </div>
-          </div>
-          {/* Desktop View */}
-          <div className="hidden lg:block sticky top-5 w-[470px] ml-auto -mr-20 mt-[161px]">
-            <ProductSelector
-              product={product}
-              accessories={accessories}
-              onVariantChange={onVariantChange}
-              onAccessoryToggle={onAccessoryToggle}
-              selectedVariant={selectedVariant}
-              selectedAccessoriesHeater={selectedAccessoriesHeater}
-              selectedAccessoriesShades={selectedAccessoriesShades}
-              selectedAccessoriesGlassdoor={selectedAccessoriesGlassdoor}
-              pergolaSize={pergolaSize}
-              pergolaQuantity={pergolaQuantity}
-              totalPrice={totalPrice}
-              onQuantityChange={onQuantityChange}
-              onBuyNow={onBuyNow}
-              isMobile={false}
-            />
           </div>
         </div>
         {/* Assembly Tab Content - Full Width */}
