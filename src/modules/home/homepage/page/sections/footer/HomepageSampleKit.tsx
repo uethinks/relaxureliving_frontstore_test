d61ee966f5f4348f0914e69dd7780943327c87a0
huangpeipei <huang.peipei@system-in-motion.com>
2025-04-22 12:54:35 +0800

    sample kit adjustment

--- src/modules/home/homepage/page/sections/footer/HomepageSampleKit.tsx
+++ src/modules/home/homepage/page/sections/footer/HomepageSampleKit.tsx
@@ -57,36 +57,40 @@ export const HomepageSampleKit = (): JSX.Element => {
   }
   console.log("sampleKitProduct", sampleKitProduct)
   return (
-    <div className="flex flex-col justify-center w-full items-center gap-5 mt-[68px] lg:px-[260px]">
-      <img
-        className="relative max-w-full lg:max-w-[563px] mt-[-10.00px] rounded-[30px] border-[10px] border-solid border-[#ffffff]"
-        alt="Rectangle"
-        src={sampleKitProduct?.images?.[0]?.url ?? undefined}
-      />
+    <div className="flex flex-col justify-center w-full items-center gap-5 mt-[68px] mb-[100px] lg:px-[260px]">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-5 relative self-stretch w-full flex-[0_0_auto]">
+        <img
+          className="relative max-w-[360px] mt-[-10.00px] rounded-[30px] border-[10px] border-solid border-[#ffffff]"
+          alt="Rectangle"
+          src={sampleKitProduct?.images?.[0]?.url ?? undefined}
+        />
 
-      <div className="flex flex-col items-center gap-5 relative self-stretch w-full flex-[0_0_auto]">
-        <div className="relative self-stretch mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-white text-[18px] lg:text-[length:var(--heading-2-font-size)] text-center tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
-          {sampleKitProduct?.title}
-        </div>
-
-        <p className="relative w-full opacity-80 font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-white text-[14px] lg:text-[length:var(--relaxure-sub-heading-18-font-size)] text-center tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
-          {sampleKitProduct?.description}
-        </p>
-      </div>
-
-      <div className="inline-flex flex-col items-center gap-5 relative flex-[0_0_auto]">
-        <button
-          onClick={() => setShowDialog(true)}
-          className="w-[271px] h-[48px] border-[#ffffff] border bg-[#072f6c] rounded-[10px] shadow-[0px_3px_7px_#072f6c1a,0px_13px_13px_#072f6c17,0px_29px_17px_#072f6c0d,0px_51px_20px_#072f6c03,0px_80px_22px_#072f6c00] overflow-hidden"
-        >
-          <div className="[font-family:'Montserrat',Helvetica] font-medium text-white text-[14px] md:text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
-            {sampleKitProduct?.subtitle}
+        <div className="flex flex-col justify-center items-center gap-5 relative self-stretch w-full flex-[0_0_auto]">
+          <div className="relative self-stretch mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-white text-[18px] lg:text-[length:var(--heading-2-font-size)] text-center tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
+            {sampleKitProduct?.title}
           </div>
-        </button>
-        <div className="relative w-fit [font-family:'Montserrat',Helvetica] font-medium text-[#ffffff] text-[14px] md:text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
-          $
-          {sampleKitProduct?.variants?.[0]?.calculated_price?.calculated_amount}{" "}
-          with Free Shipping
+
+          <p className="relative w-full opacity-80 font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-white text-[14px] lg:text-[length:var(--relaxure-sub-heading-18-font-size)] text-center tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+            {sampleKitProduct?.description}
+          </p>
+          <div className="inline-flex flex-col items-center gap-5 relative flex-[0_0_auto]">
+            <button
+              onClick={() => setShowDialog(true)}
+              className="w-[271px] h-[48px] border-[#ffffff] border bg-[#072f6c] rounded-[10px] shadow-[0px_3px_7px_#072f6c1a,0px_13px_13px_#072f6c17,0px_29px_17px_#072f6c0d,0px_51px_20px_#072f6c03,0px_80px_22px_#072f6c00] overflow-hidden"
+            >
+              <div className="[font-family:'Montserrat',Helvetica] font-medium text-white text-[14px] md:text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
+                {sampleKitProduct?.subtitle}
+              </div>
+            </button>
+            <div className="relative w-fit [font-family:'Montserrat',Helvetica] font-medium text-[#ffffff] text-[14px] md:text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
+              $
+              {
+                sampleKitProduct?.variants?.[0]?.calculated_price
+                  ?.calculated_amount
+              }{" "}
+              with Free Shipping
+            </div>
+          </div>
         </div>
       </div>
 
