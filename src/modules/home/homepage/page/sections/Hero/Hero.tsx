653c26b02891c2f43ac08968ca5f2aba0d441ef3
huangpeipei <huang.peipei@system-in-motion.com>
2025-04-24 10:17:28 +0800

    mobile homepage: the hero text should adjust the position and display one sentence

--- src/modules/home/homepage/page/sections/Hero/Hero.tsx
+++ src/modules/home/homepage/page/sections/Hero/Hero.tsx
@@ -24,12 +24,16 @@ export const Hero = ({ hero }: { hero: HeroProps | null }): JSX.Element => {
       >
         <div className="flex flex-col w-full lg:w-4/5 items-start gap-8 relative">
           <div className="flex flex-col items-start gap-6 relative flex-[0_0_auto]">
-            <p className="self-stretch mt-[-1.00px] font-heading font-[number:var(--heading-font-weight)] text-[#ffffff] text-[22px] lg:text-[length:var(--heading-font-size)] leading-[var(--heading-line-height)] relative tracking-[var(--heading-letter-spacing)] [font-style:var(--heading-font-style)]">
+            <p className="self-stretch mt-20 lg:mt-[-1.00px] font-heading font-[number:var(--heading-font-weight)] text-[#ffffff] text-[22px] lg:text-[length:var(--heading-font-size)] leading-[var(--heading-line-height)] relative tracking-[var(--heading-letter-spacing)] [font-style:var(--heading-font-style)]">
               {hero?.Title}
             </p>
 
-            <p className="self-stretch w-full lg:w-3/4 text-[#ffffff] text-[16px] lg:text-[length:var(--relaxure-sub-heading-18-font-size)] leading-[var(--relaxure-sub-heading-18-line-height)] relative font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+            <p className="hidden md:block self-stretch w-full lg:w-3/4 text-[#ffffff] text-[16px] lg:text-[length:var(--relaxure-sub-heading-18-font-size)] leading-[var(--relaxure-sub-heading-18-line-height)] relative font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
               {hero?.Description}
+            </p>
+            <p className="block md:hidden self-stretch w-full lg:w-3/4 text-[#ffffff] text-[16px] lg:text-[length:var(--relaxure-sub-heading-18-font-size)] leading-[var(--relaxure-sub-heading-18-line-height)] relative font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
+              Create memorable gatherings and intimate moments in a space that
+              impresses guests and enhances everyday living.
             </p>
           </div>
 
