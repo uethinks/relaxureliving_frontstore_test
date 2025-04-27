8a08bd40ef23376ebb9e40e9209a6a8104a0a77b
huangpeipei <huang.peipei@system-in-motion.com>
2025-04-27 20:55:37 +0800

    product cms update

--- src/modules/products/single/components/FirstScreen.tsx
+++ src/modules/products/single/components/FirstScreen.tsx
@@ -31,11 +31,11 @@ export const FirstScreen: React.FC<FirstScreenProps> = ({
             <div className="flex flex-col w-full lg:max-w-[64%] items-center gap-5 lg:pr-[65px]">
               <div className="flex flex-col items-start relative self-stretch w-full">
                 <div className="relative mb-[24px] self-stretch mt-[-1.00px] font-heading-2 font-[number:var(--heading-2-font-weight)] text-[#343a40] text-[24px] lg:text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
-                  {currentProductInfo.productSubtitle}
+                  {currentProductInfo.productTitle}
                 </div>
                 <div className="flex flex-col items-start gap-2.5 relative self-stretch w-full">
                   <p className="w-full text-[#69727a] relative font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[18px] lg:text-[22px] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
-                    {currentProductInfo.productTitle}
+                    {currentProductInfo.productSubtitle}
                   </p>
                 </div>
                 <p className="w-full text-[#69727A] relative font-relaxure-sub-heading-18 font-[number:var(--relaxure-sub-heading-18-font-weight)] text-[16px] lg:text-[length:var(--relaxure-sub-heading-18-font-size)] tracking-[var(--relaxure-sub-heading-18-letter-spacing)] leading-[var(--relaxure-sub-heading-18-line-height)] [font-style:var(--relaxure-sub-heading-18-font-style)]">
