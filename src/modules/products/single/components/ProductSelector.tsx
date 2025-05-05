09336602fdb7b9170b090ba35b24ec7dd329dea1
huangpeipei <huang.peipei@system-in-motion.com>
2025-05-05 23:18:25 +0800

    free shipping

--- src/modules/products/single/components/ProductSelector.tsx
+++ src/modules/products/single/components/ProductSelector.tsx
@@ -208,28 +208,10 @@ export const ProductSelector: React.FC<ProductSelectorProps> = ({
         <div className="flex w-full flex-col items-start gap-4 relative">
           <div className="flex w-full flex-col items-start gap-2.5 relative">
             <div className="flex justify-between items-start gap-2.5 relative self-stretch w-full">
-              <div className="flex items-center justify-center gap-2.5 px-2.5 py-0 relative">
-                <div className="flex items-end justify-start gap-4">
-                  <div className="w-fit [font-family:'Montserrat',Helvetica] font-bold text-[28px] leading-[32px] whitespace-nowrap relative tracking-[0]">
+              <div className="flex items-center justify-center gap-2.5 py-0 relative">
+                <div className="flex flex-col items-start justify-start gap-4">
+                  <div className="w-fit [font-family:'Montserrat',Helvetica] font-bold text-[36px] leading-[32px] whitespace-nowrap relative tracking-[0]">
                     ${totalPrice}
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <div className="w-fit [font-family:'Montserrat',Helvetica] font-medium text-[12px] leading-[20px] whitespace-nowrap relative text-[#6c757d] line-through">
-                      ${totalOriginalPrice}
-                    </div>
-                    {totalOriginalPrice > totalPrice && (
-                      <div className="[font-family:'Montserrat',Helvetica] px-2 py-0.5 bg-[#e9ecef] rounded-full flex items-center justify-center">
-                        <span className="text-[12px] font-normal text-[red]">
-                          Save{" "}
-                          {Math.round(
-                            ((totalOriginalPrice - totalPrice) /
-                              totalOriginalPrice) *
-                              100
-                          )}
-                          %
-                        </span>
-                      </div>
-                    )}
                   </div>
                 </div>
               </div>
@@ -254,6 +236,26 @@ export const ProductSelector: React.FC<ProductSelectorProps> = ({
                   <span className="text-[#343a40] text-lg -mt-0.5">+</span>
                 </button>
               </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="w-fit [font-family:'Montserrat',Helvetica] font-medium text-[18px] leading-[20px] whitespace-nowrap relative text-[#6c757d] line-through">
+                ${totalOriginalPrice}
+              </div>
+              {
+                <div className="[font-family:'Montserrat',Helvetica] px-2 py-0.5 bg-[#e9ecef] rounded-full flex items-center justify-center">
+                  <span className="text-[18px] font-normal text-[red]">
+                    Save{" "}
+                    {Math.round(
+                      ((totalOriginalPrice - totalPrice) / totalOriginalPrice) *
+                        100
+                    )}
+                    %
+                  </span>
+                </div>
+              }
+              <span className="text-[16px] font-normal">
+                with free shipping
+              </span>
             </div>
           </div>
           <div className="flex flex-col w-full items-end gap-2.5 relative">
