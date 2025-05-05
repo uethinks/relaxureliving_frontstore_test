319c2c79be67bf52c03df0cf924bc7e1a0ef58e8
huangpeipei <huang.peipei@system-in-motion.com>
2025-05-05 22:15:39 +0800

    advantage adjustment

--- src/modules/products/single/components/Advantage/Advantage.tsx
+++ src/modules/products/single/components/Advantage/Advantage.tsx
@@ -1,60 +1,113 @@
 export const Advantage = (): JSX.Element => {
   return (
-    <div className="flex w-full flex-col h-[95px] lg:h-[154px] items-start gap-2.5 px-2 lg:px-[57px] py-2.5 lg:py-5 relative self-stretch">
-      <div className="absolute w-full h-[95px] lg:h-[154px] top-0 left-0 bg-[#f3f3f3] rounded-[20px] overflow-hidden shadow-shadow-relaxure-button">
+    <div className="relative w-full flex flex-col items-start gap-2.5 px-2 lg:px-5 py-2.5 lg:py-5 self-stretch overflow-hidden">
+      <div className="absolute inset-0 w-full h-full bg-[#f3f3f3] rounded-[20px] overflow-hidden z-0" />
+      <div className="flex justify-between items-center w-full relative z-10">
         <img
-          className="absolute w-5 h-10 lg:w-10 lg:h-[76px] top-[29px] left[10px] lg:top-[39px] lg:left-[37px]"
+          className="h-10 w-5 lg:h-[76px] lg:w-10 mr-1 lg:mr-5"
           alt="Vector"
           src="/img/leave_right.svg"
         />
-
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-2.5 gap-y-1 lg:gap-x-9 lg:gap-y-0 flex-1 w-full">
+          <div
+            className="flex flex-col items-center relative"
+            style={{ height: "100px" }}
+          >
+            <div
+              className="flex items-end justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <img
+                className="w-5 h-5 lg:w-10 lg:h-[30px]"
+                alt="Frame"
+                src="/img/two_hours_installation.svg"
+              />
+            </div>
+            <div
+              className="flex items-start justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <div className="[font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
+                2 hour installation
+              </div>
+            </div>
+          </div>
+          <div
+            className="flex flex-col items-center relative"
+            style={{ height: "100px" }}
+          >
+            <div
+              className="flex items-end justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <img
+                className="w-[30px] h-[34px] lg:w-10 lg:h-[62px]"
+                alt="Frame"
+                src="/img/five_years_warranty.svg"
+              />
+            </div>
+            <div
+              className="flex items-start justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <div className="[font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
+                15 years warranty
+              </div>
+            </div>
+          </div>
+          <div
+            className="flex flex-col items-center relative"
+            style={{ height: "100px" }}
+          >
+            <div
+              className="flex items-end justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <img
+                className="w-[30px] h-[34px] lg:w-10 lg:h-[62px]"
+                alt="Frame"
+                src="/img/five_years_warranty.svg"
+              />
+            </div>
+            <div
+              className="flex items-start justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <div className="[font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
+                100 day risk free trail
+              </div>
+            </div>
+          </div>
+          <div
+            className="flex flex-col items-center relative"
+            style={{ height: "100px" }}
+          >
+            <div
+              className="flex items-end justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <img
+                className="w-5 h-5 lg:w-10 lg:h-[30px]"
+                alt="Frame"
+                src="/img/two_months_delivery.svg"
+              />
+            </div>
+            <div
+              className="flex items-start justify-center w-full"
+              style={{ height: "50px" }}
+            >
+              <div className="[font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
+                <p>Free delivery</p>
+                <p>Full insurance</p>
+              </div>
+            </div>
+          </div>
+        </div>
         <img
-          className="absolute w-5 h-10 lg:w-10 lg:h-[76px] top-[29px] right-[10px] lg:top-[39px] lg:right-[37px]"
+          className="h-10 w-5 lg:h-[76px] lg:w-10 ml-1 lg:ml-5"
           alt="Vector"
           src="/img/leave_left.svg"
         />
-      </div>
-
-      <div className="w-full flex items-center justify-center gap-2.5 lg:gap-9 relative flex-[0_0_auto]">
-        <div className="flex flex-col w-[30%] lg:w-1/3 items-center gap-1 relative">
-          <img
-            className="relative w-5 h-5 lg:w-10 lg:h-[30px]"
-            alt="Frame"
-            src="/img/two_hours_installation.svg"
-          />
-
-          <div className="relative w-full [font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
-            2 hour installation
-          </div>
-        </div>
-
-        <div className="flex w-[30%] lg:w-1/3 items-center justify-center gap-2.5 relative">
-          <div className="flex flex-col w-full items-center gap-1 relative ml-[-15.00px] mr-[-15.00px]">
-            <img
-              className="relative w-[30px] h-[34px] lg:w-10 lg:h-[62px]"
-              alt="Frame"
-              src="/img/five_years_warranty.svg"
-            />
-
-            <div className="relative w-full [font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
-              15 years warranty
-            </div>
-          </div>
-        </div>
-
-        <div className="flex w-[30%] lg:w-1/3 items-center justify-center gap-2.5 relative">
-          <div className="flex flex-col w-full items-center gap-1 relative ml-[-15.00px] mr-[-15.00px]">
-            <img
-              className="relative w-5 h-5 lg:w-10 lg:h-[30px]"
-              alt="Frame"
-              src="/img/two_months_delivery.svg"
-            />
-
-            <div className="relative w-full [font-family:'Montserrat',Helvetica] font-medium text-[#69727a] text-base text-center tracking-[0] leading-6">
-              Fast delivery
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   )
