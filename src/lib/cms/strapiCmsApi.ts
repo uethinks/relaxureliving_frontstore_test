e597c79c8f25449a096d758b67210b9bb7bfe73f
huangpeipei <huang.peipei@system-in-motion.com>
2025-06-18 19:05:21 +0800

    Revert "Custom Pergola Landing"
    This reverts commit d6685e315edae28d9d8f0ddc77887d7690057eb7.

--- src/lib/cms/strapiCmsApi.ts
+++ src/lib/cms/strapiCmsApi.ts
@@ -50,11 +50,7 @@ const glassdoorPopulate = {
   "populate[productInformations]": "*",
   "populate[product_images][fields][0]": "*",
 };
-const landingPagePopulate = {
-  "populate[hero][populate][0]": "BackgroundImage",
-  "populate[landing_slider][populate][images][fields][0]": "*",
-  "populate[good_memory][populate][0]": "image",
-};
+
 export const API_URLS = {
   getHomePage: '/api/home-page',
   getWarranty: '/api/warranty',
@@ -70,7 +66,6 @@ export const API_URLS = {
   getShades: '/api/shades-screen',
   getGlassdoor: '/api/glass-door',
   sendKlaviyoTrackInfo: '/api/contact-submissions',
-  getLandingPage: '/api/landing',
 };
 
 // 获取所有项目
@@ -230,14 +225,3 @@ export const sendKlaviyoContactUsForm = async (info: any) => {
   }
 }
 
-export const getLandingPage = async () => {
-  try {
-    const response = await axiosInstance.get(API_URLS.getLandingPage, {
-      params: landingPagePopulate
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching landing page data:', error, API_URLS.getLandingPage);
-    throw error;
-  }
-};
