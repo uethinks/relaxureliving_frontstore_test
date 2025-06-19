0fa9b42186832d0bd7b841ad1be9de6d3ff5837c
huangpeipei <huang.peipei@system-in-motion.com>
2025-06-19 15:21:45 +0800

    update and test for github sync

--- src/app/[countryCode]/(main)/landing/page.tsx
+++ src/app/[countryCode]/(main)/landing/page.tsx
@@ -74,7 +74,7 @@ export default async function LandingPage() {
   if (!landingPageData) {
     return (
       <div className="flex items-center justify-center min-h-screen">
-        <p>please try again later</p>
+        <p>please try again later!</p>
       </div>
     )
   }
