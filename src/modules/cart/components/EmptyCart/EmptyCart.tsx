a6d45c4130d1993a297fa0f829d0679e06144880
huangpeipei <huang.peipei@system-in-motion.com>
2025-07-14 10:54:09 +0800

    update the color

--- src/modules/cart/components/EmptyCart/EmptyCart.tsx
+++ src/modules/cart/components/EmptyCart/EmptyCart.tsx
@@ -16,7 +16,7 @@ export const EmptyCart = () => {
       <Link href="/products/pergola">
         <Button
           variant="primary"
-          className="h-[48px] px-8 text-[16px] text-white lg:text-[18px] font-medium font-montserrat bg-[#072F6C] hover:bg-[#0a3980] transition-colors"
+          className="h-[48px] px-8 text-[16px] text-black lg:text-[18px] font-medium font-montserrat bg-[#F6AF1F] hover:bg-[#fdce6f] transition-colors"
         >
           Explore The Corsica
         </Button>
