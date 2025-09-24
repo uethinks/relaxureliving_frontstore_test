1d2f4c1f3438401c24045839df7f18bf889eff55
Yue Li <343938328@qq.com>
2025-09-24 13:48:51 +0800

    fix: V2ServiceSnapShots mobile style

--- src/components/V2ServiceSnapshots.tsx
+++ src/components/V2ServiceSnapshots.tsx
@@ -3,7 +3,7 @@ import { getStrapiUrl } from "@lib/utils"
 interface FeatureItem {
   id: number
   title: string
-  description: string
+  description2: string
   icon: {
     id: number
     name: string
@@ -20,35 +20,50 @@ interface FeaturesSectionProps {
   items?: FeatureItem[]
 }
 
-export default function FeaturesSection({ data }: { data: FeaturesSectionProps }) {
+export default function FeaturesSection({
+  data,
+}: {
+  data: FeaturesSectionProps
+}) {
   const features = data.items || []
 
   return (
-    <section className="w-full h-[380px] flex items-center justify-center" style={{ background: 'linear-gradient(90deg, rgba(239, 238, 235, 0.50) 0%, rgba(255, 255, 255, 0.50) 100%)' }}>
-      <div className="max-w-[1074px] w-full">
-        <div className="flex flex-nowrap gap-2 justify-center w-full">
-          {features.map((feature) => (
-            <div key={feature.id} className="basis-full sm:basis-1/3 lg:basis-1/6 grow flex flex-col items-center">
-              <div className="flex justify-center mb-[28px]">
-                {feature.icon && (
-                  <img
-                    src={getStrapiUrl(feature.icon.url)}
-                    alt={feature.icon.alternativeText || feature.title}
-                    width={feature.icon.width}
-                    height={feature.icon.height}
-                    className="w-14 h-14 object-contain"
-                  />
-                )}
-              </div>
-              <h3 className="text-[#000000] font-semibold text-[16px] mb-[20px] w-[160px] text-center">
+    <section
+      className="w-full lg:h-[380px] flex items-center justify-center"
+      style={{
+        background:
+          "linear-gradient(90deg, rgba(239, 238, 235, 0.50) 0%, rgba(255, 255, 255, 0.50) 100%)",
+      }}
+    >
+      <div className="p-6 pb-12 lg:p-0 lg:max-w-[1074px] w-full flex flex-wrap lg:flex-nowrap gap-6 justify-center">
+        {features.map((feature) => (
+          <div
+            key={feature.id}
+            className="basis-full lg:basis-1/6 grow flex lg:flex-col items-center lg:gap-7 gap-8"
+          >
+            {feature.icon && (
+              <img
+                src={getStrapiUrl(feature.icon.url)}
+                alt={feature.icon.alternativeText || feature.title}
+                width={feature.icon.width}
+                height={feature.icon.height}
+                className="w-14 h-14 object-contain flex-shrink-0 flex-grow-0 basis-auto"
+              />
+            )}
+            <div
+              className={
+                "flex flex-col lg:text-center gap-2 lg:gap-5 w-full lg:w-40"
+              }
+            >
+              <h3 className="text-[#2F2A1E] text-base font-semibold line-clamp-2">
                 {feature.title}
               </h3>
-              <p className="text-[#8c877c] text-sm whitespace-pre-line w-[170px] text-center">
-                {feature.description}
+              <p className="text-[#8c877c] text-sm whitespace-pre-line line-clamp-2 lg:line-clamp-3">
+                {feature.description2}
               </p>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </section>
   )
