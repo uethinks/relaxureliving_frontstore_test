4fc53878d82049235afa22fc16e0a7646cef9dee
Yue Li <343938328@qq.com>
2025-09-24 14:05:55 +0800

    fix: V2HeroProductSection mobile style

--- src/components/V2HeroProductSection.tsx
+++ src/components/V2HeroProductSection.tsx
@@ -1,9 +1,7 @@
-import { ArrowRight } from "lucide-react"
-import Image from "next/image"
-import { Button } from "@/components/ui/button"
 import { getStrapiUrl } from "@lib/utils"
-import V2Headline from "./V2Headline"
+import Image from "next/image"
 import V2Button from "./V2Button"
+import V2Headline from "./V2Headline"
 
 interface ButtonData {
   id: number
@@ -58,11 +56,11 @@ export default function V2HeroProductSection({
   const { title, description, button, image } = data
 
   return (
-    <section className="w-full h-[480px] flex items-center justify-center">
-      <div className="max-w-[1074px] w-full h-full grid grid-cols-1 lg:grid-cols-2 gap-16">
+    <section className="w-full h-auto lg:h-[480px] flex lg:items-center lg:justify-center">
+      <div className="max-w-[1074px] w-full h-auto lg:h-full grid grid-cols-1 lg:grid-cols-2 lg:gap-16">
         {/* Left side - Pergola Image */}
-        <div className="h-[480px] overflow-hidden">
-          <Image  
+        <div className="w-full h-auto lg:h-[480px] overflow-hidden">
+          <Image
             unoptimized
             src={getStrapiUrl(image.url)}
             alt={
@@ -71,13 +69,13 @@ export default function V2HeroProductSection({
             }
             width={image.width}
             height={image.height}
-            className="object-cover h-full w-full"
+            className="w-full h-auto lg:h-full object-cover"
             // priority -- remove it for lazy loading, as it is above the fold
           />
         </div>
 
         {/* Right side - Content */}
-        <div className="space-y-8 flex flex-col justify-center items-start">
+        <div className="space-y-8 flex flex-col justify-center items-start w-full px-6 pt-5 pb-[50px] lg:p-0">
           <header className="space-y-4">
             <V2Headline title={title} as="h1" size="md" />
           </header>
