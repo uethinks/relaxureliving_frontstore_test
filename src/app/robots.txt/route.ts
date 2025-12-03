3cf18caca6f19f7fedbf5b0961aa5f09de4f7d65
white.li <white.li@coherencedigital.com>
2025-12-03 15:45:13 +0800

    fix: update robots.txt

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
   const robotsTxt = isProduction
     ? `User-agent: *
 Allow: /
-Sitemap: https://www.relaxureliving.com/api/strapi-5-sitemap-plugin/sitemap.xml`
+Sitemap: https://www.relaxureliving.com/sitemap.xml`
     : `User-agent: *
 Disallow: /`;
 
