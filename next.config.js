33b622f5a9fe1abcfcfca90ab5da43f4bfef1991
white.li <white.li@coherencedigital.com>
2026-01-23 11:24:03 +0800

    fix: test2 rewrites in next config

--- next.config.js
+++ next.config.js
@@ -27,11 +27,11 @@ const nextConfig = {
     return [
       {
         source: "/api/data/:match*",
-        destination: "https://relaxureliving-frontstore-test.vercel.app/_vercel/insights/:match*",
+        destination: "https://test2.relaxureliving.com/_vercel/insights/:match*",
       },
       {
         source: "/api/performance/:match*",
-        destination: "https://relaxureliving-frontstore-test.vercel.app/_vercel/speed-insights/:match*",
+        destination: "https://test2.relaxureliving.com/_vercel/speed-insights/:match*",
       },
     ];
   },
